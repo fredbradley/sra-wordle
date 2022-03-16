7fdf7b36b679d94e15f10b49449bed51cd8b7626
cranweb <support@cranleigh.org>
2022-03-16 10:16:11 +0000

    lint

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the colour of the tiles will
-        change to show how close your guess was to the word.
+        Guess the word in 6 tries. After each guess, the colour of the tiles
+        will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -58,12 +58,22 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-          You can suggest a word for the list: <a rel="noreferrer" target="_blank" className="underline font-bold" href="https://docs.google.com/forms/d/e/1FAIpQLSdt99k1MKEWQQpQqXlRlaZmV6MRT7NODPNdkMnjYS5uMHTwiA/viewform?usp=sf_link">Fill in the form.</a>
+        You can suggest a word for the list:{' '}
+        <a
+          rel="noreferrer"
+          target="_blank"
+          className="underline font-bold"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdt99k1MKEWQQpQqXlRlaZmV6MRT7NODPNdkMnjYS5uMHTwiA/viewform?usp=sf_link"
+        >
+          Fill in the form.
+        </a>
       </p>
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        If you want to try making your own version speak to FRB. You can
-          {' '}<a
-          target="_blank" rel="noreferrer" href="https://github.com/cwackerfuss/react-wordle"
+        If you want to try making your own version speak to FRB. You can{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
           check out the source code here
